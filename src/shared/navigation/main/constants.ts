fa13269c7db0e7186f3586179c657ac2d0d66993
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-07-27 17:25:43 -0500

    feat: implement bottom tap navigation main

---
+++ src/shared/navigation/main/constants.ts
@@ -0,0 +1,6 @@
+import {BottomTabNavigationOptions} from '@react-navigation/bottom-tabs';
+
+export const SCREEN_OPTIONS: BottomTabNavigationOptions = {
+  headerShown: false,
+  animation: 'none',
+};
