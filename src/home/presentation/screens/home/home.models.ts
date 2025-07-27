ee87692b8869337a3c29408694e56b0532a8efca
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-07-27 17:23:50 -0500

    feat: implement home feature

---
+++ src/home/presentation/screens/home/home.models.ts
@@ -0,0 +1,6 @@
+import {NavigationModels} from '@shared/navigation';
+
+export namespace HomeScreenModels {
+  export type PresenterProps =
+    NavigationModels.RootBottomTapScreenProps<NavigationModels.MAIN_ROUTES_NAMES.HOME>;
+}
