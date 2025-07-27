d9c95acbbebb71a692a2453d0a2d8f3454682c25
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-07-27 17:24:14 -0500

    feat: implement marketplace feature

---
+++ src/marketplace/presentation/screens/marketplace/marketplace.presenter.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import {styles} from './marketplace.styles';
+
+const MarketplacePresenter = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Screen Marketplace</Text>
+    </View>
+  );
+};
+
+export default MarketplacePresenter;
