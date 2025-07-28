0782c581574e2495ded03ab606b2ee059526dd97
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-07-28 10:54:46 -0500

    feat: add eslint rule for exhaustive dependencies in React hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
     'react-native/split-platform-components': 'warn',
     'react-native/no-color-literals': 'warn',
     'react-native/no-single-element-style-arrays': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
