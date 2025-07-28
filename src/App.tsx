ab3daaf6d8b2d3546d14f436fad5db3ee738d7c0
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-07-28 10:57:17 -0500

    feat: wrap NavigationTree with SafeAreaProvider and ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationTree} from '@shared/navigation';
+import {ThemeProvider} from '@shared/context/theme';
 
 export default function App() {
-  return <NavigationTree />;
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <NavigationTree />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
 }
