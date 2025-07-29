37c5ff8fceeb57e2331a0d3343034b59fed3c4a3
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-07-28 23:57:34 -0500

    fix: implement theme in StatusBar component

--- src/shared/navigation/navigation.tree.tsx
+++ src/shared/navigation/navigation.tree.tsx
@@ -1,11 +1,18 @@
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from './main';
+import {useTheme} from '@shared/hooks';
 
-const NavigationTree = () => (
-  <NavigationContainer>
-    <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
-    <MainNavigation />
-  </NavigationContainer>
-);
+const NavigationTree = () => {
+  const {isDark, colors} = useTheme();
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle={isDark ? 'light-content' : 'dark-content'}
+        backgroundColor={colors.background}
+      />
+      <MainNavigation />
+    </NavigationContainer>
+  );
+};
 export default NavigationTree;
