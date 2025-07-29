781f64a24c11de8feaff758fa9ab4a0ede4c6646
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-07-28 23:59:09 -0500

    feat: implement home screen

--- src/home/presentation/screens/home/home.models.ts
+++ src/home/presentation/screens/home/home.models.ts
@@ -1,6 +1,19 @@
 import {NavigationModels} from '@shared/navigation';
+import {PostEntity} from '@home/domain/entities';
 
 export namespace HomeScreenModels {
   export type PresenterProps =
     NavigationModels.RootBottomTapScreenProps<NavigationModels.MAIN_ROUTES_NAMES.HOME>;
+
+  export type ComponentProps = {
+    data?: Array<PostEntity>;
+    isLoading?: boolean;
+    isFirstLoading?: boolean;
+    isError?: boolean;
+    onErrorRetry: () => void;
+  };
+
+  export type RenderItem = {
+    item: PostEntity;
+  };
 }
--- src/home/presentation/screens/home/home.styles.ts
+++ src/home/presentation/screens/home/home.styles.ts
@@ -1,9 +1,31 @@
-import {StyleSheet} from 'react-native';
+import {useMemo} from 'react';
+import {StyleSheet, Dimensions} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useTheme} from '@shared/hooks';
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const {height} = Dimensions.get('window');
+
+export function useStyles() {
+  const {colors, isDark} = useTheme();
+  const insets = useSafeAreaInsets();
+  return useMemo(
+    () =>
+      StyleSheet.create({
+        container: {
+          flex: 1,
+          paddingTop: insets.top,
+          backgroundColor: colors.background,
+        },
+        ctnEmpty: {
+          height: height * 0.8,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        itemSeparator: {
+          width: '100%',
+          height: 10,
+        },
+      }),
+    [isDark],
+  );
+}
